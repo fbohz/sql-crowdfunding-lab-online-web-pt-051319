2514b6c6a01db19901087a1c7af4c4bc502e13be
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-07-16 17:14:02 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -6,4 +6,11 @@ INSERT INTO users (
 VALUES ( "felipe", 22 ), ( "felipe", 22 ),  ( "felipe", 22 ), ( "felipe", 22 ), ( "felipe", 22 ), ( "felipe", 22 ),  ( "felipe", 22 ), ( "felipe", 22 ), ( "felipe", 22 ), ( "felipe", 22 ),  ( "felipe", 22 ), ( "felipe", 22 ), ( "felipe", 22 ), ( "felipe", 22 ),  ( "felipe", 22 ), ( "felipe", 22 ), ( "felipe", 22 ), ( "felipe", 22 ),  ( "felipe", 22 ), ( "felipe", 22 ); 
 
 
-INSERT INTO projects
+INSERT INTO projects (
+ title,
+ category,
+ funding_goal
+)
+
+VALUES ("pipe", "environment", 66552), ("pipe", "environment", 66552), ("pipe", "environment", 66552), ("pipe", "environment", 66552), ("pipe", "environment", 66552), ("pipe", "environment", 66552), ("pipe", "environment", 66552), ("pipe", "environment", 66552), ("pipe", "environment", 66552), ("pipe", "environment", 66552); 
+
